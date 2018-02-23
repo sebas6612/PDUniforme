452b5fe49b4cabf4acd6bb27edf6e2cde8d4e61a
sebas6612 <sebas6612@gmail.com>
2018-02-23 18:15:54 -0500

    corrección de error en path

--- PuntoDelUniforme/urls.py
+++ PuntoDelUniforme/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from PuntoDelUniforme.Apps.Facturación.views import PDFPrueba
-from IPython.external.path import path as Path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
